a2360540f1b418e0801034010d87b9ed0f68c414
An Huynh <huynha1@wit.edu>
2021-08-10 23:37:30 -0400

    fix: updated descriptions and added experience from J.H. under WorkExperiences page

--- src/container/WorkExperiences/index.js
+++ src/container/WorkExperiences/index.js
@@ -8,36 +8,43 @@ const workExperiencesList = [
     title: "Staples, Inc",
     date: "June 2017 - Auguest 2017",
     subtitle: "Digital Solutions Intern",
-    description: "AngularJs, Javascript, REST APIs, Python",
+    description: "AngularJs, Javascript, REST APIs, and Python",
   },
   {
     icon: "down-circle",
     title: "Visible Systems Corporation",
     date: "September 2017 - December 2017",
     subtitle: "Product and Platform Developer Intern",
-    description: "Big Data Applications (Hadoop + Presto), Mendix, AWS Athena and Quicksight",
+    description: "Hadoop, Presto, Mendix, AWS Athena, and AWS Quicksight",
   },
   {
     icon: "down-circle",
     title: "Eaton Vance",
     date: "January 2018 - May 2018",
     subtitle: "IT Distribution Systems Intern",
-    description: "Angular 5, PL/SQL, PHP, Oracle Database",
+    description: "Angular 5, PL/SQL, PHP, and Oracle Database",
   },
   {
     icon: "down-circle",
     title: "Pegasystems",
     date: "May 2018 - August 2018",
     subtitle: "Junior System Architect Intern",
-    description: "Jenkins, Python, Agile-Scrum methodology",
+    description: "Jenkins, Python, and Agile-Scrum methodology",
   },
   {
     icon: "down-circle",
     title: "Amazon Robotics",
     date: "September 2018 - December 2018",
     subtitle: "Solution Analytics Intern",
-    description: "Java, AngularJs, Webpack, Coral Service Framework, AWS Lambda, DynamoDB",
-  }
+    description: "Java, AngularJs, Webpack, Coral Service Framework, AWS Lambda, and AWS DynamoDB",
+  },
+  {
+    icon: "down-circle",
+    title: "John Hancock",
+    date: "October 2019 - June 2020",
+    subtitle: "Data Engineer",
+    description: "Java, Scala, SQL/HQL, Hadoop, Hive, Shell/Bash, Apache Ranger, Maven, Azure, and CA Workload Automation.",
+  },
 ];
 
 class WorkExperiences extends Component {
