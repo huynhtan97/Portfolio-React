fef6b00a4d045068dac4abda40ca61e8640db7f9
An Huynh <huynha1@wit.edu>
2021-08-10 22:30:05 -0400

    fix: updated projects page to replace 2 projects and added new images

--- src/container/Projects/index.js
+++ src/container/Projects/index.js
@@ -3,36 +3,30 @@ import { Row, Col, Card, Button } from "antd";
 import MainLayout from "../../layout/MainLayout";
 
 import sheFightsBack from "../../assets/images/sheFightsBack.png";
-import imageRecognition from "../../assets/images/imageRecognitionApp.png";
-import chatBot from "../../assets/images/chatBotApp.jpg";
+import safetyNet from "../../assets/images/techtogetherDevPostSubmission.png";
+import aaaiSubmission from "../../assets/images/AAAILogo.jpeg";
 import everlead from "../../assets/images/everlead.png";
 
 const projects = [
   {
     title: "shefightsback",
-    description: `Winner of Most Innovative and She Learns (Best Beginners) prize at SheHacks 2018. Mobile application aimed to
-          allow victims of domestic abuse to efficiently report incidents and reach out for help without risk of personal safety`,
     image: sheFightsBack,
     link: "https://github.com/huynhtan97/sheFightsBack",
   },
   {
-    title: "IBM Cloud Image Recognition",
-    description: `Application built with Node-Red and IBM Cloud's Image Recognition API that allows users to identify the gender and age 
-                  of a person based on their image url.`,
-    image: imageRecognition,
-    link: "https://github.com/huynhtan97/nodeRedFaceRecognition",
+    title: "SafetyNet",
+    image: safetyNet,
+    link: "https://github.com/huynhtan97/safetynetWeb",
   },
   {
-    title: "IBM Cloud Chat bot",
-    description: `AI Chat bot built using IBM Cloud's Watson Assistant and Tone Analyzer API, as well as APIXU's Weather API. Current 
-                  functionalities include: - Greetings - Telling Jokes - Getting user's name - Informing you on weather - Changing 
-                  responses based on user's tone in text. Hoping to add: - Personality Analyzer API - More features with Tone Analyzer`,
-    image: chatBot,
-    link: "https://github.com/huynhtan97/mood-bot",
+    title: "AAAI Research Submission",
+    image: aaaiSubmission,
+    link: "https://huynhtan97.github.io/Portfolio/assets/AAAI_Gesture_Abstract.pdf",
+    buttonText: "Paper",
+    buttonIcon: "book"
   },
   {
     title: "Everlead",
-    description: `Website built for my past LeaderShape mentor using MaterializeCSS`,
     image: everlead,
     link: "https://github.com/huynhtan97/Everlead",
   }
@@ -45,7 +39,7 @@ class Projects extends Component {
           {projects.map(project => (
             <Col key={project.title} xl={12} lg={12} md={12} xs={24} sm={24} style={{}}>
               <Card                                                                
-                actions={[<a href={project.link}><Button type="primary" shape="round" icon="github" size="large">Source Code</Button></a>]}
+                actions={[<a href={project.link}><Button type="primary" shape="round" icon={project.buttonIcon || "github"} size="large" style={{ width: 200 }}>{project.buttonText || "Source Code"}</Button></a>]}
                 cover={<img src={project.image} alt={project.title} style={{ height: "400px", overflow: "hidden", objectFit: "cover"}}/>}                
               >
                 <Card.Meta 
